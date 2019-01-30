605000b170dcc0e1d1b474e7e848c38837a9cdd9
Rob Paskin <rjpaskin+git@gmail.com>
2019-01-30 20:12:45 +0000

    Add basic Emacs config with packages.el setup

---
+++ Mackup/.emacs.d/init.el
@@ -0,0 +1,18 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(package-initialize)
+
+;; Download the ELPA archive description if needed.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
