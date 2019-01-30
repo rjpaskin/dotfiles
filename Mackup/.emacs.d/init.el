e03343a8eaf760e968f4964f4d277072e6bcafce
Rob Paskin <rjpaskin+git@gmail.com>
2019-01-30 20:17:58 +0000

    Add packages suggested by starter-kit and Clojure for the Brave & True
    - Emacs Starter Kit: https://github.com/technomancy/emacs-starter-kit/blob/v3/README.markdown
    - Clojure for the Brave & True: https://github.com/flyingmachine/emacs-for-clojure/blob/44d0d5b6f/init.el

--- Mackup/.emacs.d/init.el
+++ Mackup/.emacs.d/init.el
@@ -16,3 +16,44 @@
 
 (eval-when-compile
   (require 'use-package))
+
+(use-package better-defaults
+  :ensure t)
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package paredit
+  :ensure t)
+
+(use-package clojure-mode
+  :ensure t)
+
+(use-package clojure-mode-extra-font-locking
+  :ensure t)
+
+(use-package cider
+  :ensure t
+  :pin melpa-stable)
+
+;; allow ido usage in as many contexts as possible
+(use-package ido-completing-read+
+  :ensure t
+  :config
+  (ido-ubiquitous-mode 1))
+
+;; Enhances M-x to allow easier execution of commands. Provides
+;; a filterable list of possible commands in the minibuffer
+(use-package smex
+  :ensure t)
+
+(use-package projectile
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+(use-package magit
+  :ensure t)
