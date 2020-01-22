c042d1199a9b44ecaf209a8e1a6ea2f2634e9b81
Rob Paskin <rjpaskin+git@gmail.com>
2020-01-22 21:09:45 +0000

    Add Emacs custom.el

---
+++ Mackup/.emacs.d/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking paredit exec-path-from-shell better-defaults use-package)))
+ '(quelpa-update-melpa-p nil)
+ '(safe-local-variable-values
+   (quote
+    ((cider-figwheel-main-default-options . ":dev")
+     (cider-default-cljs-repl . figwheel-main)
+     (cider-default-cljs-repl quote figwheel-main)
+     (cider-clojure-cli-global-options . "-A:fig")
+     (cider-default-cljs-repl . node)
+     (cider-preferred-build-tool . lein)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
