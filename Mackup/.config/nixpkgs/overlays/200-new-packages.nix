61c23d04bf2a9e31d4f5509b50a1fd76eb45b9c2
Rob Paskin <rjpaskin+git@gmail.com>
2020-02-26 23:49:49 +0000

    Add WIP derivation for Heroku with plugins
    Resulting wrapped executable can't find plugins :-(.
    
    TODO: try another approach?

--- Mackup/.config/nixpkgs/overlays/200-new-packages.nix
+++ Mackup/.config/nixpkgs/overlays/200-new-packages.nix
@@ -7,6 +7,7 @@ in {
   autoterm = super.callPackage ../pkgs/autoterm.nix { inherit ruby; };
   dockutil = super.callPackage ../pkgs/dockutil.nix {};
   git-when-merged = super.callPackage ../pkgs/git-when-merged.nix {};
+  heroku-with-plugins = super.callPackage ../pkgs/heroku-with-plugins.nix {};
   mackup = super.python38.pkgs.callPackage ../pkgs/mackup.nix {};
   nix-rebuild = super.callPackage ../pkgs/nix-rebuild.nix {};
 }
--- Mackup/.config/nixpkgs/overlays/300-common.nix
+++ Mackup/.config/nixpkgs/overlays/300-common.nix
@@ -19,6 +19,7 @@ self: super:
       mackup
       ncdu
       universal-ctags
+      heroku-with-plugins
       ;
   };
 }
