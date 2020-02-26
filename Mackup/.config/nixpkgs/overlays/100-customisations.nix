8a471654e5cc3ae173e12f966a11214ac05e0c0d
Rob Paskin <rjpaskin+git@gmail.com>
2020-02-26 23:23:31 +0000

    Prevent extra outputs from being GC-ed
    Stops the cycle of:
    
    1. `nix-collect-garbage` removes outputs
    2. `nix-rebuild` re-downloads them
    3. GOTO 1
    
    TODO: find a better way to do this that doesn't put the extra outputs
    (like *every* cert from cacerts) into the user profile.

---
+++ Mackup/.config/nixpkgs/overlays/100-customisations.nix
@@ -0,0 +1,13 @@
+self: super:
+
+{
+  # Prevent extra outputs from being GC-ed, since they just get
+  # redownloaded the next time nix-rebuild is run
+  cacert = super.cacert.overrideAttrs (attrs: {
+    meta.outputsToInstall = (attrs.meta.outputsToInstall or []) ++ [ "out" "unbundled" ];
+  });
+
+  jq = super.jq.overrideAttrs (attrs: {
+    meta.outputsToInstall = (attrs.meta.outputsToInstall or []) ++ [ "out" "bin" "man" "dev" "doc" ];
+  });
+}
