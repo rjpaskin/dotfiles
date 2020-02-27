95a236e0e557383a5e3a5ca74d8fbe3cfd4cafe3
Rob Paskin <rjpaskin+git@gmail.com>
2020-02-27 00:09:29 +0000

    Only link main executable for gems/Python packages

--- Mackup/.config/nixpkgs/pkgs/autoterm.nix
+++ Mackup/.config/nixpkgs/pkgs/autoterm.nix
@@ -1,8 +1,16 @@
-{ buildRubyGem, ruby }:
+{ buildRubyGem, ruby, linkFarm }:
 
-buildRubyGem rec {
-  inherit ruby;
+let
   gemName = "autoterm";
-  version = "0.2.0";
-  source.sha256 = "0s7ha0k1c0p95i6i24zcw4nv8yv2hssxz658chcrqa6m7ys12691";
-};
+
+  src = buildRubyGem rec {
+    inherit ruby gemName;
+    version = "0.2.0";
+    source.sha256 = "0s7ha0k1c0p95i6i24zcw4nv8yv2hssxz658chcrqa6m7ys12691";
+  };
+
+  name = "bin/${gemName}";
+in
+  linkFarm gemName [
+    { inherit name; path = "${src}/${name}"; }
+  ]
--- Mackup/.config/nixpkgs/pkgs/ultrahook.nix
+++ Mackup/.config/nixpkgs/pkgs/ultrahook.nix
@@ -1,11 +1,19 @@
-{ buildRubyGem, ruby }:
+{ buildRubyGem, ruby, linkFarm }:
 
-# ultrahook has a dependency on the `json` gem,
-# but we don't include it since it's present in the
-# Ruby standard library
-buildRubyGem rec {
-  inherit ruby;
-  version = "0.1.3";
+let
   gemName = "ultrahook";
-  source.sha256 = "1dbwbd5d20jjffq8lwzpwx9wa2larm1n3r527p6kxl9w9rwsvnc1";
-};
+
+  # ultrahook has a dependency on the `json` gem,
+  # but we don't include it since it's present in the
+  # Ruby standard library
+  src = buildRubyGem rec {
+    inherit ruby gemName;
+    version = "0.1.3";
+    source.sha256 = "1dbwbd5d20jjffq8lwzpwx9wa2larm1n3r527p6kxl9w9rwsvnc1";
+  };
+
+  name = "bin/${gemName}";
+in
+  linkFarm gemName [
+    { inherit name; path = "${src}/${name}"; }
+  ]
