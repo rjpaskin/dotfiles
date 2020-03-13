1001a66ff6257b61d45715038af3ef1d86aeba07
Rob Paskin <rjpaskin+git@gmail.com>
2020-03-13 21:28:12 +0000

    Add "roles" modules
    To serve the same function as system tags do now.

---
+++ Mackup/.config/nixpkgs/modules/roles.nix
@@ -0,0 +1,24 @@
+{ lib, ... }:
+
+with lib;
+with types;
+
+let
+  mkRole = default: name: mkOption {
+    inherit default;
+    description = "Enable `${name}` role";
+    type = bool;
+  };
+
+in
+  {
+    options.roles = {
+      description = "Top-level roles config";
+      type = attrsOf bool;
+    };
+
+    config.lib.roles = {
+      mkOptionalRole = mkRole false;
+      mkDefaultRole = mkRole true;
+    };
+  }
