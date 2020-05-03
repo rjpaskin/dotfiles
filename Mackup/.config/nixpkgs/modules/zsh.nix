43730f58d33dd03422ced85e2da674fcc4e7d13c
Rob Paskin <rjpaskin+git@gmail.com>
2020-05-03 20:46:37 +0100

    Enable HM ZSH module and add basic config

--- Mackup/.config/nixpkgs/modules/zsh.nix
+++ Mackup/.config/nixpkgs/modules/zsh.nix
@@ -1,9 +1,34 @@
-{ config, ... }:
+{ config, lib, ... }:
 
-{
-  home.symlinks = config.lib.mackup.mackupFiles [
-    ".config/zsh/.zshrc"
-    ".config/zsh/.zshrc.local"
-    ".zshenv"
-  ];
+let
+  dag = lib.hm.dag;
+
+in {
+  programs.zsh = {
+    enable = true;
+    dotDir = ".config/zsh";
+    enableCompletion = true;
+
+    history = {
+      path = ".local/share/zsh/history";
+      # Match oh-my-zsh settings
+      extended = true;
+      expireDuplicatesFirst = true;
+      ignoreDups = true;
+      ignoreSpace = true;
+      size = 50000;
+      save = 10000;
+      share = true;
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+
+      plugins = [
+        "history-substring-search"
+        "osx"
+      ];
+    };
+  };
 }
