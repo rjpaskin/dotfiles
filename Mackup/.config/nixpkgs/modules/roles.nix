877574f6c1adeff36db065dd6d0b6fbb65d047ea
Rob Paskin <rjpaskin+git@gmail.com>
2020-05-08 21:07:25 +0100

    Extract new HM roles from tags used in zshrc and other shell files
    Ultimate goal is to replace all usages of tags - first in shell files,
    and then in Brewfile - with HM roles.

--- Mackup/.config/nixpkgs/modules/roles.nix
+++ Mackup/.config/nixpkgs/modules/roles.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 with lib;
 with types;
@@ -10,15 +10,23 @@ let
     type = bool;
   };
 
-in
-  {
-    options.roles = {
-      description = "Top-level roles config";
-      type = attrsOf bool;
-    };
+in {
+  options.roles = {
+    description = "Top-level roles config";
+    type = attrsOf bool;
+  };
 
-    config.lib.roles = {
-      mkOptionalRole = mkRole false;
-      mkDefaultRole = mkRole true;
-    };
-  }
+  config.lib.roles = {
+    mkOptionalRole = mkRole false;
+    mkDefaultRole = mkRole true;
+  };
+
+  options.roles.docker = config.lib.roles.mkOptionalRole "Docker tools";
+  options.roles.git = config.lib.roles.mkOptionalRole "Git and tools";
+  options.roles.git-flow = config.lib.roles.mkOptionalRole "Git flow";
+  options.roles.go = config.lib.roles.mkOptionalRole "Golang dev";
+  options.roles.heroku = config.lib.roles.mkOptionalRole "Heroku tools";
+  options.roles.javascript = config.lib.roles.mkOptionalRole "Javascript dev";
+  options.roles.react-native = config.lib.roles.mkOptionalRole "React Native dev (Android)";
+  options.roles.ruby = config.lib.roles.mkOptionalRole "Ruby dev";
+}
