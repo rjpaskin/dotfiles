07f7495fbe420fdc41e17b18710cf7f6bdcf71fb
Rob Paskin <rjpaskin+git@gmail.com>
2020-06-07 21:58:25 +0100

    Source `ag` from Nix, rather than Homebrew

--- spec/home_manager/pkgs_spec.rb
+++ spec/home_manager/pkgs_spec.rb
@@ -1,6 +1,19 @@
 RSpec.describe "Packages" do
   let(:nix_profile_manpath) { profile_path("share/man") }
 
+  describe program("ag") do
+    its(:location) { should eq profile_bin }
+    its(:manpage) { should be_inside nix_profile_manpath }
+
+    let(:needle) { "string for ag to find" }
+
+    it "runs without errors" do
+      result = run_in_shell!("ag --vimgrep '#{needle}' '#{File.dirname __FILE__}'")
+
+      expect(result).to include(__FILE__, needle)
+    end
+  end
+
   describe program("jq") do
     its(:location) { should eq profile_bin }
     its(:manpage) { should be_inside nix_profile_manpath }
