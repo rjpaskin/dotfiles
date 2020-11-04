cc4531f4c3c45578b5b7a86a63714651a806acdb
Rob Paskin <rjpaskin+git@gmail.com>
2020-11-04 11:57:52 +0000

    Update Vim plugins
    `vim-ruby-refactoring` now uses `main` instead of `master`, so track
    that instead.

--- Mackup/.config/nixpkgs/pkgs/vim-plugins/generated.nix
+++ Mackup/.config/nixpkgs/pkgs/vim-plugins/generated.nix
@@ -6,13 +6,15 @@ let
 {
   splitjoin-vim = buildVimPluginFrom2Nix {
     pname = "splitjoin-vim";
-    version = "2020-02-08";
+    version = "2020-10-25";
     src = fetchFromGitHub {
       owner = "AndrewRadev";
       repo = "splitjoin.vim";
-      rev = "6bfdc927f858a514d7b9802dffe56be90d06ac10";
-      sha256 = "05qifigwd3dl4jzzjgn13crzhzm7cw0bzxrsppkp7drza32r5hmd";
+      rev = "1c1b94a6aa218c429421c82bcc56a216301b6e85";
+      sha256 = "1jkny1ag82zvkfjvbzrkkh4s54jcf9hq5n9ak3g691zcddhmrp17";
+      fetchSubmodules = true;
     };
+    meta.homepage = "https://github.com/AndrewRadev/splitjoin.vim/";
   };
 
   vim-alias = buildVimPluginFrom2Nix {
@@ -24,17 +26,19 @@ let
       rev = "f0aa2bf9fbaa9e0d16c4b6d32841a126e41b9202";
       sha256 = "17cay94gvaqvxhq3vij2f8pcyfpgrf74lhwbwpwfciwqs9czg0hw";
     };
+    meta.homepage = "https://github.com/Konfekt/vim-alias/";
   };
 
   vim-bundler = buildVimPluginFrom2Nix {
     pname = "vim-bundler";
-    version = "2019-11-13";
+    version = "2020-09-26";
     src = fetchFromGitHub {
       owner = "tpope";
       repo = "vim-bundler";
-      rev = "0396ce134dd69b022c20b104c450c6121f0cb760";
-      sha256 = "0crxib110l3d9vg63glbynxdifrawnsayhvjfqqiaiv31dz78q5h";
+      rev = "40efd19c0a4447ff2f142d3d89735ac3d637a355";
+      sha256 = "1vwvm708cdrrlyc1ys2i9qj2mv2mrcp183jgpn1bq0nnpz2w09w1";
     };
+    meta.homepage = "https://github.com/tpope/vim-bundler/";
   };
 
   vim-jsx = buildVimPluginFrom2Nix {
@@ -46,6 +50,7 @@ let
       rev = "8879e0d9c5ba0e04ecbede1c89f63b7a0efa24af";
       sha256 = "0czjily7kjw7bwmkxd8lqn5ncrazqjsfhsy3sf2wl9ni0r45cgcd";
     };
+    meta.homepage = "https://github.com/mxw/vim-jsx/";
   };
 
   vim-mkdir = buildVimPluginFrom2Nix {
@@ -57,28 +62,31 @@ let
       rev = "f0ba7a7dc190a0cedf1d827958c99f3718109cf0";
       sha256 = "0kp2n1wfmlcxcwpqp63gmzs8ihdhd5qcncc4dwycwr1sljklarnw";
     };
+    meta.homepage = "https://github.com/pbrisbin/vim-mkdir/";
   };
 
   vim-prettier = buildVimPluginFrom2Nix {
     pname = "vim-prettier";
-    version = "2020-02-18";
+    version = "2020-11-03";
     src = fetchFromGitHub {
       owner = "prettier";
       repo = "vim-prettier";
-      rev = "7041b9cb8bee84f432f1e168f826a90a0beea93a";
-      sha256 = "13zfirw9n0bimcjh694sg98g4ixvv6jwja0zc6qnxkynwwygz63f";
+      rev = "bc7ae99c38a1d0f58380347515b212f93df5e68e";
+      sha256 = "1czdzrhlsac08lgb0chqr1nbkwacggp74s02vfv3xsa1cqj3b1pw";
     };
+    meta.homepage = "https://github.com/prettier/vim-prettier/";
   };
 
   vim-rails = buildVimPluginFrom2Nix {
     pname = "vim-rails";
-    version = "2020-01-17";
+    version = "2020-09-29";
     src = fetchFromGitHub {
       owner = "tpope";
       repo = "vim-rails";
-      rev = "64befc6187678893082bebb8be79c1d17fdd07ba";
-      sha256 = "1yjqxqx3g74afzdq8iqd6zw7ypn6dv1i14qi9a7jnnwnp956lc12";
+      rev = "2c42236cf38c0842dd490095ffd6b1540cad2e29";
+      sha256 = "0nhf4qd7dchrzjv2ijcddav72qb121c9jkkk06agsv23l9rb31pv";
     };
+    meta.homepage = "https://github.com/tpope/vim-rails/";
   };
 
   vim-rspec = buildVimPluginFrom2Nix {
@@ -90,6 +98,7 @@ let
       rev = "52a72592b6128f4ef1557bc6e2e3eb014d8b2d38";
       sha256 = "09prk06rrbs8pgfm4iz88sp151p6pi9bl76p6macvv5nxv72d9j8";
     };
+    meta.homepage = "https://github.com/thoughtbot/vim-rspec/";
   };
 
   vim-ruby-refactoring = buildVimPluginFrom2Nix {
@@ -101,6 +110,7 @@ let
       rev = "6447a4debc3263a0fa99feeab5548edf27ecf045";
       sha256 = "1fgwpfmzy3mfcx4cyfhmxk42np9g0bgp1nrc0w7vr9wlmzwn7mch";
     };
+    meta.homepage = "https://github.com/ecomba/vim-ruby-refactoring/";
   };
 
   vim-rubyhash = buildVimPluginFrom2Nix {
@@ -112,6 +122,7 @@ let
       rev = "d020a8eeac40a55617a72aa135b702b6da0c9b62";
       sha256 = "16shl7ww5qj4vqaai7crbsrrcvpdgrgww3n00lkzfyfp1chnhhd1";
     };
+    meta.homepage = "https://github.com/rorymckinley/vim-rubyhash/";
   };
 
   vim-textobj-rubyblock = buildVimPluginFrom2Nix {
@@ -123,6 +134,7 @@ let
       rev = "2b882e2cc2599078f75e6e88cd268192bf7b27bf";
       sha256 = "197m0a45ywdpq2iqng6mi5qjal2qggjjww1k95iz2s7wvh8ng9yr";
     };
+    meta.homepage = "https://github.com/nelstrom/vim-textobj-rubyblock/";
   };
 
   vim-textobj-variable-segment = buildVimPluginFrom2Nix {
@@ -134,17 +146,19 @@ let
       rev = "78457d4322b44bf89730e708b62b69df48c39aa3";
       sha256 = "14dcrnk83hj4ixrkdgjrk9cf0193f82wqckdzd4w0b76adf3habj";
     };
+    meta.homepage = "https://github.com/Julian/vim-textobj-variable-segment/";
   };
 
   vim-yaml-helper = buildVimPluginFrom2Nix {
     pname = "vim-yaml-helper";
-    version = "2018-04-15";
+    version = "2020-03-11";
     src = fetchFromGitHub {
       owner = "lmeijvogel";
       repo = "vim-yaml-helper";
-      rev = "db031682d339f361e2f8ed213cdb2c505cd0e011";
-      sha256 = "03c5xx7ikahkj5ngihjfznc3d3cfb7sw9lw2wlwvmmj9rinq8zil";
+      rev = "403ff568e336def133b55d25a3f9517f406054cc";
+      sha256 = "038f29hkq0xi60jyxmciszd1ssjkinc3zqhp25a2qk08qsigyg9f";
     };
+    meta.homepage = "https://github.com/lmeijvogel/vim-yaml-helper/";
   };
 
 });
