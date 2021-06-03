b694c25273af66f68695c66ed158907ef2f15feb
Rob Paskin <rjpaskin+git@gmail.com>
2021-06-03 23:14:38 +0100

    Split out Homebrew HM module into defs and my config
    Simplifies files (making them easier to follow, less indentation) and
    makes definition easier to reuse.

---
+++ modules/homebrew/default.nix
@@ -0,0 +1,98 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  inherit (config) roles;
+
+in {
+  imports = [ ./options.nix ];
+
+  options.roles = with config.lib.roles; {
+    cyberduck = mkOptionalRole "Cyberduck";
+    dropbox = mkOptionalRole "Dropbox";
+    eqmac = mkOptionalRole "eqMac";
+    gimp = mkOptionalRole "GIMP";
+    harvest = mkOptionalRole "Harvest taskbar app";
+    inkscape = mkOptionalRole "Inkscape";
+    ngrok = mkOptionalRole "ngrok";
+    postman = mkOptionalRole "Postman";
+    slack = mkOptionalRole "Slack";
+    sql-clients = mkOptionalRole "SQL clients (Sequel Pro, TablePlus)";
+    virtualbox = mkOptionalRole "Virtual Box with extensions";
+    whatsapp = mkOptionalRole "WhatsApp";
+    zoom = mkOptionalRole "Zoom app";
+  };
+
+  config.targets.darwin.homebrew = {
+    casks = mkMerge [
+      [
+        "google-chrome"
+        "firefox"
+
+        {
+          name = "atom";
+          files = [
+            "atom/config.cson"
+            "atom/init.coffee"
+            "atom/keymap.cson"
+            "atom/snippets.cson"
+            "atom/styles.less"
+          ];
+          prefs = ["com.github.atom"];
+        }
+        {
+          name = "dash";
+          prefs = ["com.kapeli.dash" "com.kapeli.dashdoc"];
+          files = ["Library/Application Support/Dash/library.dash"];
+        }
+        { name = "emacs"; files = ["emacs.d"]; }
+        { name = "iterm2"; prefs = ["com.googlecode.iterm2"]; }
+        "kdiff3"
+        "xquartz"
+
+        # Quicklook plugins
+        {
+          name = "qlcolorcode"; # syntax highlighting
+          defaults."org.n8gray.QLColorCode".pathHL = "${pkgs.highlight}/bin/highlight";
+        }
+        "qlcommonmark" # markdown files
+        "qlstephen" # files without extensions
+        "quicklook-json"
+        "quicklook-csv"
+
+        { name = "1password"; prefs = ["com.agilebits.onepassword4"]; }
+        { name = "keepingyouawake"; prefs = ["info.marcel-dierkes.KeepingYouAwake"]; }
+        "betterzip"
+        "imageoptim"
+        "mollyguard"
+        "superduper"
+        "vlc"
+      ]
+
+      (mkIf roles.cyberduck ["cyberduck"])
+      (mkIf roles.dropbox ["dropbox"])
+      (mkIf roles.eqmac [{ name = "eqmac"; prefs = ["com.bitgapp.eqMac2"]; }])
+      (mkIf roles.gimp ["gimp"])
+      (mkIf roles.inkscape ["inkscape"])
+      (mkIf roles.ngrok ["ngrok"])
+      (mkIf roles.postman ["postman"])
+      (mkIf roles.slack ["slack"])
+      (mkIf roles.sql-clients ["sequel-pro" "tableplus"])
+      (mkIf roles.virtualbox ["virtualbox" "virtualbox-extension-pack"])
+      (mkIf roles.whatsapp ["whatsapp"])
+      (mkIf roles.zoom ["zoom"])
+    ];
+
+    masApps = mkMerge [
+      {
+        Keynote = 409183694;
+        Numbers = 409203825;
+        Pages = 409201541;
+        "HP Smart" = 1474276998;
+      }
+
+      (mkIf roles.harvest { Harvest = 506189836; })
+    ];
+  };
+}
