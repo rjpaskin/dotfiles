9d9769b88a488aa69fea504cca87e787932d163f
Rob Paskin <rjpaskin+git@gmail.com>
2021-07-27 20:14:00 +0100

    Add host for M1 MacBook Pro (running macOS 11 "Big Sur")

---
+++ hosts/cr-m1.nix
@@ -0,0 +1,34 @@
+{ lib, ... }:
+
+with lib;
+
+{
+  roles = {
+    aws = true;
+    clojure = true;
+    docker = true;
+    eqmac = true;
+    flight-plan = true;
+    git = true;
+    git-standup = true;
+    harvest = true;
+    heroku = true;
+    javascript = true;
+    ngrok = true;
+    postman = true;
+    react-native = true;
+    rubocop = true;
+    ruby = true;
+    slack = true;
+    sql-clients = true;
+    tmux = true;
+    whatsapp = true;
+    zoom = true;
+  };
+
+  programs.zsh.initExtra = mkAfter ''
+    [ -n "$IN_AUTOTERM_SPEC" ] && HISTFILE=""
+  '';
+
+  # ruby.rubocop.package = pkgs.rubocop_0_59;
+}
