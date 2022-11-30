38020dfccad1eb7506af92e3c6f138410fe38f9a
Rob Paskin <rjpaskin+git@gmail.com>
2022-11-30 22:09:40 +0000

    Bump HM state version to latest

--- home.nix
+++ home.nix
@@ -15,7 +15,7 @@ with lib;
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    home.stateVersion = "20.09";
+    home.stateVersion = "23.05";
 
     news.display = "silent";
 
