f205000e97d41906b437e2e0a8c1a663d198616d
Rob Paskin <rjpaskin+git@gmail.com>
2023-11-05 23:21:35 +0000

    Use "Dynamic Profile" for iTerm
    Allows us to just write a text file in order to override iTerm settings,
    rather than having to make plist edits, and have said file be the source
    of truth for settings (iTerm doesn't allow a dynamic profile to be
    edited via the Preferences UI).

--- modules/iterm.nix
+++ modules/iterm.nix
@@ -1,8 +1,6 @@
 { config, ... }:
 
 let
-  domain = "com.googlecode.iterm2";
-
   toColour = parts: {
     "Blue Component" = builtins.elemAt parts 2;
     "Green Component" = builtins.elemAt parts 1;
@@ -12,33 +10,46 @@ let
   white = toColour [1 1 1];
   black = toColour [0 0 0];
 
+  profileUUID = "A00868BC-8749-41C1-8B9E-B92BDA34E275";
+
 in {
   targets.darwin = {
     homebrew.casks = [ "iterm2" ];
 
-    defaults.${domain} = {
+    defaults."com.googlecode.iterm2" = {
       AlternateMouseScroll = true;
-      Columns = 150;
-      "Custom Directory" = "Recycle";
+      "Default Bookmark Guid" = profileUUID;
       EnableAPIServer = true; # enable Python API
-      "Normal Font" = "Monaco 13";
       OpenArrangementAtStartup = false;
       PreserveWindowSizeWhenTabBarVisibilityChanges = true;
       SUEnableAutomaticChecks = true;
-      "Scrollback Lines" = 0; # unlimited
       ShowNewOutputIndicator = false;
       SoundForEsc = false;
       StretchTabsToFillBar = false;
       TabStyleWithAutomaticOption = 4;
-      "Unlimited Scrollback" = true;
       VisualIndicatorForEsc = false;
-
-      "Background Color" = white;
-      "Bold Color" = black;
-      "Cursor Color" = black;
-      "Foreground Color" = black;
     };
+  };
 
-    plists."Library/Preferences/${domain}.plist".":New Bookmarks:0:Allow Title Setting" = false;
+  # https://iterm2.com/documentation-dynamic-profiles.html
+  home.file."Library/Application Support/iTerm2/DynamicProfiles/rjp.json".text = builtins.toJSON {
+    Profiles = [
+      {
+        Name = "RJP";
+        Guid = profileUUID;
+
+        "Allow Title Setting" = false;
+        "Columns" = 150;
+        "Custom Directory" = "Recycle";
+        "Scrollback Lines" = 0; # unlimited
+        "Unlimited Scrollback" = true;
+        "Normal Font" = "Monaco 13";
+
+        "Background Color" = white;
+        "Foreground Color" = black;
+        "Bold Color" = black;
+        "Cursor Color" = black;
+      }
+    ];
   };
 }
