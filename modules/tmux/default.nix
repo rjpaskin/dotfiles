70b3bd7d1aec76bd524ea2e063891e5ec581b1cc
Rob Paskin <rjpaskin+git@gmail.com>
2025-09-23 21:42:44 +0100

    Remove references to Tmuxinator
    Not used for a long time - `autoterm` largely fills that niche now. If I
    were to start using a terminal multiplexer again, I'd probably look into
    Zellij, which has built-in layout config.

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -9,21 +9,10 @@ with lib;
   };
 
   config = mkMerge [
-    {
-      # `~/..tmuxinator/*.yml` files are used by autoterm so always need be present
-      home.file = {
-        ".tmuxinator/default.yml".source = ./tmuxinator/default.yml;
-        ".tmuxinator/default_helper.rb".source = ./tmuxinator/default_helper.rb;
-      };
-
-      privateLinks."tmuxinator/*.yml" = ".tmuxinator";
-    }
-
     (mkIf config.roles.tmux {
       home.packages = with pkgs; [
         reattach-to-user-namespace
         tmux
-        tmuxinator
       ];
 
       home.file.".tmux.conf".source = ./tmux.conf;
--- spec/home_manager/tmux_spec.rb
+++ spec/home_manager/tmux_spec.rb
@@ -1,29 +1,4 @@
 RSpec.describe "Tmux" do
-  describe home_path(".tmuxinator"), pending: "Needed?" do
-    it { should be_a_directory.and be_readable }
-
-    it "contains YAML config files" do
-      configs = subject.glob("*.yml").grep_v(/default\.yml$/)
-
-      aggregate_failures do
-        expect(configs).to_not be_empty
-        expect(configs).to all have_attributes(
-          readable?: true,
-          blank?: false,
-          in_nix_store?: false
-        )
-      end
-    end
-
-    describe home_path(".tmuxinator/default.yml") do
-      it { should be_a_file.and be_readable }
-    end
-
-    describe home_path(".tmuxinator/default_helper.rb") do
-      it { should be_a_file.and be_readable }
-    end
-  end
-
   context "when enabled", role: "tmux" do
     describe program("tmux") do
       its(:location) { should eq profile_bin }
@@ -31,20 +6,6 @@ RSpec.describe "Tmux" do
 
       it "runs without errors" do
         result = run_in_shell("tmux list-commands")
-
-        aggregate_failures do
-          expect(result).to be_success
-          expect(result.stdout.lines.size).to be > 1
-          expect(result.stderr).to be_empty
-        end
-      end
-    end
-
-    describe program("tmuxinator") do
-      its(:location) { should eq profile_bin }
-
-      it "runs without errors" do
-        result = run_in_shell("tmuxinator list")
 
         aggregate_failures do
           expect(result).to be_success
