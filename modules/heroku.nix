71f500c33e4fa6c5b8da401078474544e918956b
Rob Paskin <rjpaskin+git@gmail.com>
2026-02-02 21:01:27 +0000

    Remove Heroku plugins code
    heroku-accounts is deprecated/archived, and heroku-repo functionality
    can be done via the Heroku API (I believe).
    
    Would probably just install plugins 'normally' if I were ever to add
    some in future.
    
    TODO: reintroduce `modules/heroku.nix` as multi-module.

--- modules/heroku.nix
+++ modules/heroku.nix
@@ -2,34 +2,14 @@
 
 with lib;
 
-let
-  cfg = config.programs.heroku;
-
-  plugins = import ../pkgs/heroku-plugins {
-    inherit pkgs;
-    inherit (pkgs) nodejs;
-  };
-
-  herokuWithPlugins = pkgs.callPackage ../pkgs/heroku-with-plugins.nix {};
-
-in {
+{
   options = with config.lib.roles; {
     roles.heroku = mkOptionalRole "Heroku tools";
-
-    programs.heroku.plugins = mkOption {
-      type = types.listOf types.package;
-      description = "List of plugins to bundle into Heroku package";
-    };
   };
 
   config = mkIf config.roles.heroku {
     home.packages = [
-      (herokuWithPlugins cfg.plugins)
-    ];
-
-    programs.heroku.plugins = with plugins; [
-      heroku-accounts
-      heroku-repo
+      pkgs.heroku
     ];
 
     programs.zsh.oh-my-zsh.plugins = ["heroku"];
--- spec/home_manager/heroku_spec.rb
+++ spec/home_manager/heroku_spec.rb
@@ -1,8 +1,4 @@
 RSpec.describe "Heroku", role: "heroku" do
-  let(:heroku_commands) do
-    run_in_shell!("heroku commands --output=json").as_json.map {|cmd| cmd[:id] }
-  end
-
   describe program("heroku") do
     its(:location) { should eq nix_profile_bin }
     its("--version") { should be_success.and include("heroku") }
@@ -10,72 +6,5 @@ RSpec.describe "Heroku", role: "heroku" do
 
   describe oh_my_zsh_plugins do
     it { should include("heroku") }
-  end
-
-  describe "plugins" do
-    let(:data_dir) { xdg_data_path("heroku") }
-    let(:plugins) { %w[heroku-accounts heroku-repo] }
-
-    let(:package_json) do
-      begin
-        data_dir.join("package.json").as_json
-      rescue Errno::ENOENT # don't care if package.json is present
-        { dependencies: [] }
-      end
-    end
-
-    let(:yarn_lock) do
-      begin
-        data_dir.join("yarn.lock").lines
-      rescue Errno::ENOENT
-        []
-      end
-    end
-
-    it "does not have plugins installed in $HOME" do
-      aggregate_failures do
-        plugins.each do |plugin|
-          expect(data_dir.join "node_modules", plugin).to be_absent
-          expect(package_json[:dependencies]).not_to include(plugin)
-          expect(yarn_lock).not_to include(plugin)
-        end
-      end
-    end
-
-    describe "heroku-accounts" do
-      it "is installed" do
-        expect(heroku_commands).to include("accounts", "accounts:add", "accounts:set")
-      end
-    end
-
-    describe "heroku-repo" do
-      it "is installed" do
-        expect(heroku_commands).to include("repo:purge_cache", "repo:gc")
-      end
-    end
-  end
-
-  context "disabling updates" do
-    let(:package_directory) { nix_profile_bin("heroku").realpath.dirname.join("..") }
-
-    it "removes `update` command" do
-      aggregate_failures do
-        expect(heroku_commands).not_to include("update")
-        expect(run_in_shell! "heroku").not_to include(/^  update/)
-      end
-    end
-
-    it "removes update plugins from Heroku config" do
-      package_json = package_directory.join("package.json").as_json
-
-      aggregate_failures do
-        expect(package_json[:dependencies]).not_to include(
-          "@oclif/plugin-warn-if-update-available",
-          "@oclif/plugin-update"
-        )
-
-        expect(package_json[:oclif][:topics]).not_to include("update")
-      end
-    end
   end
 end
